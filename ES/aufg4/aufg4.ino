c48da78ccd84ed027d1fe89029f203b4cf009d45
IrateGod <konamijudge@yahoo.com>
2018-06-08 12:14:27 +0200

    Add aufg4, more logic fixes -- move debounce resets into loop rather than timer

--- ES/aufg4/aufg4.ino
+++ ES/aufg4/aufg4.ino
@@ -13,12 +13,16 @@ auto motor_input_2 = 49;
 bool motor_mode;
 int incButtonActiveMSCount;
 int decButtonActiveMSCount;
+int bothButtonActiveMSCount;
 int incButtonReleasedMSCount;
 int decButtonReleasedMSCount;
+int bothButtonReleasedMSCount;
 bool isIncButtonPressed;
 bool isDecButtonPressed;
+bool isBothButtonPressed;
 bool inc_button_pressed;
 bool dec_button_pressed;
+bool both_button_pressed;
 char intensity;
 int intensity_step = 16;
 int minThreshold = 10;
@@ -26,6 +30,8 @@ int rotation;
 
 DueTimer dec_timer;
 DueTimer inc_timer;
+DueTimer both_timer;
+
 
 void changeMode()
 {
@@ -39,8 +45,7 @@ void changeMode()
 void checkDecButtonState(void)
 {
   int current_button_state = digitalRead(dec_button_pin);
-  int current_inc_state = digitalRead(inc_button_pin);
-  if (current_button_state == LOW && current_inc_state == HIGH)
+  if (current_button_state == LOW)
   {
     decButtonActiveMSCount += 1;
   }
@@ -56,21 +61,30 @@ void checkDecButtonState(void)
   if (decButtonActiveMSCount > minThreshold)
   {
     isDecButtonPressed = true;
-    decButtonActiveMSCount = 0;
+    if (isIncButtonPressed)
+    {
+      isBothButtonPressed = true;
+      if (decButtonReleasedMSCount > minThreshold && incButtonReleasedMSCount > minThreshold)
+      {
+        Serial.println("both released");
+        both_button_pressed = true;
+        inc_button_pressed = false;
+        dec_button_pressed = false;
+        isBothButtonPressed = false;
+      }
+    }
   }
-  if (decButtonReleasedMSCount > minThreshold)
+  if (decButtonReleasedMSCount > minThreshold && !isBothButtonPressed)
   {
     dec_button_pressed = true;
     isDecButtonPressed = false;
-    decButtonReleasedMSCount = 0;
   }
 }
 
 void checkIncButtonState(void)
 {
   int current_button_state = digitalRead(inc_button_pin);
-  int current_dec_state = digitalRead(dec_button_pin);
-  if (current_button_state == LOW && current_dec_state == HIGH)
+  if (current_button_state == LOW)
   {
     incButtonActiveMSCount += 1;
   }
@@ -86,13 +100,41 @@ void checkIncButtonState(void)
   if (incButtonActiveMSCount > minThreshold)
   {
     isIncButtonPressed = true;
-    incButtonActiveMSCount = 0;
   }
-  if (incButtonReleasedMSCount > minThreshold)
+  if (incButtonReleasedMSCount > minThreshold && !isBothButtonPressed)
   {
     inc_button_pressed = true;
     isIncButtonPressed = false;
-    incButtonReleasedMSCount = 0;
+  }
+}
+
+void checkBothButtonState(void)
+{
+  int current_inc_state = digitalRead(inc_button_pin);
+  int current_dec_state = digitalRead(dec_button_pin);
+  if (current_inc_state == LOW && current_dec_state == LOW)
+  {
+    bothButtonActiveMSCount += 1;
+  }
+  else if (current_inc_state == HIGH && current_dec_state == HIGH && isBothButtonPressed)
+  {
+    bothButtonReleasedMSCount += 1;
+  }
+  else
+  {
+    bothButtonActiveMSCount = 0;
+    bothButtonReleasedMSCount = 0;
+  }
+  if (bothButtonActiveMSCount > minThreshold)
+  {
+    isBothButtonPressed = true;
+    bothButtonActiveMSCount = 0;
+  }
+  if (bothButtonReleasedMSCount > minThreshold)
+  {
+    both_button_pressed = true;
+    isBothButtonPressed = false;
+    bothButtonReleasedMSCount = 0;
   }
 }
 
@@ -104,8 +146,10 @@ void setup() {
   decButtonReleasedMSCount = 0;
   isIncButtonPressed = false;
   isDecButtonPressed = false;
+  isBothButtonPressed = false;
   inc_button_pressed = false;
   dec_button_pressed = false;
+  both_button_pressed = false;
   intensity = 64;
   rotation = ROT_CW;
   Serial.begin(115200);
@@ -122,11 +166,27 @@ void setup() {
   {
     inc_timer.start();
   }
+  /*if (both_timer.configure(1000, checkBothButtonState))
+  {
+    both_timer.start();
+  }*/
 }
 
 void loop() {
-  if (dec_button_pressed && !inc_button_pressed)
+  if (both_button_pressed)
   {
+    changeMode();
+    dec_button_pressed = false;
+    inc_button_pressed = false;
+    both_button_pressed = false;
+    decButtonActiveMSCount = 0;
+    decButtonReleasedMSCount = 0;
+    incButtonActiveMSCount = 0;
+    incButtonReleasedMSCount = 0;
+  }
+  else if (dec_button_pressed && !inc_button_pressed)
+  {
+   Serial.println("press dec button");
     if (motor_mode == CH_ROT)
     {
       if (rotation == ROT_CCW)
@@ -146,9 +206,13 @@ void loop() {
     {
       intensity = intensity - intensity_step;
     }
+    dec_button_pressed = false;
+    decButtonActiveMSCount = 0;
+    decButtonReleasedMSCount = 0;
   }
   else if (!dec_button_pressed && inc_button_pressed)
   {
+    Serial.println("press inc button");
     if (motor_mode == CH_ROT)
     {
       if (rotation == ROT_CW)
@@ -168,14 +232,17 @@ void loop() {
     {
      intensity = intensity + intensity_step; 
     }
+    inc_button_pressed = false;
+    incButtonActiveMSCount = 0;
+    incButtonReleasedMSCount = 0;
   }
-  else if (dec_button_pressed && inc_button_pressed)
-  {
-    changeMode();
-  }
-  dec_button_pressed = false;
-  inc_button_pressed = false;
   analogWrite(motor_pwm, intensity);
+  /*switch (motor_mode)
+  {
+    case CH_ROT: analogWrite(led, GREEN); break;
+    case CH_INT: analogWrite(led, RED); break;
+    default: break;
+  }*/
   switch (rotation)
   {
     case ROT_CW: digitalWrite(motor_input_2, HIGH); break;
